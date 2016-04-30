b1e8510cdc478dac37b4e117fd3821b668d47460
Andre Caron <andre.l.caron@gmail.com>
2016-04-30 14:22:56 -0400

    Adds CLI for HTTP endpoint.

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -10,3 +10,19 @@ def test_version(capsys):
     assert error.value.args[0] == 0
     stdout, _ = capsys.readouterr()
     assert stdout.strip() == version
+
+
+def test_defaults(capsys):
+    arguments = cli.parse_args([])
+    assert arguments.host == '0.0.0.0'
+    assert arguments.port == 8080
+
+
+def test_host(capsys):
+    arguments = cli.parse_args(['--host=127.0.0.1'])
+    assert arguments.host == '127.0.0.1'
+
+
+def test_port(capsys):
+    arguments = cli.parse_args(['--port=80'])
+    assert arguments.port == 80
