748572159ba5d5e779c60f2a92977e1da1db0a73
Andre Caron <andre.l.caron@gmail.com>
2016-09-17 23:13:14 -0400

    Adds support for sending logs to FluentD.

--- tests/conftest.py
+++ tests/conftest.py
@@ -2,8 +2,10 @@
 
 import aiohttp
 import aiohttp.web
+import aiotk
 import asyncio
 import functools
+import msgpack
 import os
 import os.path
 import pytest
@@ -11,10 +13,11 @@ import shutil
 import structlog
 import random
 import tempfile
+import testfixtures
 import unittest.mock
 import urllib.parse
 
-from smartmob_agent import autoclose, responder
+from smartmob_agent import autoclose, responder, configure_logging
 from unittest import mock
 
 __here__ = os.path.dirname(os.path.abspath(__file__))
@@ -107,6 +110,16 @@ def temp_folder():
     shutil.rmtree(path)
 
 
+@pytest.yield_fixture(scope='function')
+def tempdir():
+    old_cwd = os.getcwd()
+    with testfixtures.TempDirectory(create=True) as directory:
+        os.chdir(directory.path)
+        yield
+        os.chdir(old_cwd)
+        directory.cleanup()
+
+
 class MockSubprocess(object):
     """Mock implementation of asyncio ``Popen`` object."""
 
@@ -181,3 +194,53 @@ def subprocess_factory():
     with unittest.mock.patch('asyncio.create_subprocess_exec') as spawn:
         spawn.side_effect = create_subprocess_exec
         yield factory
+
+
+@pytest.fixture(scope='function', autouse=True)
+def logging():
+    """Setup default logging for tests.
+
+    Tests can reconfigure logging if they wish to.
+    """
+    configure_logging(
+        log_format='kv',
+        utc=False,
+        endpoint='file:///dev/stdout',
+    )
+
+
+async def service_fluent_client(records, reader, writer):
+    """TCP handler for mock FluentD server.
+
+    See:
+    - https://github.com/fluent/fluentd/wiki/Forward-Protocol-Specification-v0
+    - https://pythonhosted.org/msgpack-python/api.html#msgpack.Unpacker
+    """
+    unpacker = msgpack.Unpacker()
+    data = await reader.read(1024)
+    while data:
+        unpacker.feed(data)
+        for record in unpacker:
+            records.append(record)
+        data = await reader.read(1024)
+
+
+@pytest.yield_fixture(scope='function')
+def fluent_server(event_loop, unused_tcp_port):
+    """Mock FluentD server."""
+
+    records = []
+
+    # TODO: provide a built-in means to pass in shared server state as this
+    #       wrapper will probably not cancel cleanly.
+    async def service_connection(reader, writer):
+        return await service_fluent_client(records, reader, writer)
+
+    # Serve connections.
+    host, port = ('127.0.0.1', unused_tcp_port)
+    server = aiotk.TCPServer(host, port, service_connection)
+    server.start()
+    event_loop.run_until_complete(server.wait_started())
+    yield host, port, records
+    server.close()
+    event_loop.run_until_complete(server.wait_closed())
